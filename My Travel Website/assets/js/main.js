e653b6f264d4d533d4f660b5665998d41490c22b
sanjaykrishnank <717823i245@kce.ac.in>
2024-05-09 07:33:42 +0530

    Add files via upload

---
+++ My Travel Website/assets/js/main.js
@@ -0,0 +1,90 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/*============ MENU SHOW ===========*/
+// validate if constant exits//
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+     // *MENU HIDDEN*  //
+// validate if constant exits//
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById("nav-menu")
+        //    when we click each nav__link, we remove the show-menu class 
+         navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // when the scroll is greater than 350 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+             const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight, 
+              sectionTop = current.offsetTop -58,
+              sectionId= current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+              sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+
+        }
+    })
+
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+   // reset: true // animation repeat
+})
+
+sr.reveal('.home__data, , .explore__data, .explore__user')
+sr.reveal('.home__card', {delay:600, distance: '100px', interval: 100})
+sr.reveal('.about__data, .join__image', {origin: 'right'})
+sr.reveal('.about__image, .join__data' , {origin: 'left'})
+sr.reveal('.popular__card', {interval: 200})
